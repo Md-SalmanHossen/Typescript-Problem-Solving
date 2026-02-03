b48c4754846380109c576cba968faa00e02ff725
SalmanHossen <hossainsalman873@gmail.com>
2026-02-03 18:27:00 +0600

    practice first variable problem in ts

---
+++ 1.Common_Porblems/1Variable-conditions/1Positive_negative.js
@@ -0,0 +1,8 @@
+var num = -5;
+console.log("Given number is: ", num);
+if (num > 0)
+    console.log("positive number");
+else if (num < 0)
+    console.log("negative number");
+else
+    console.log("the number is zero");
---
+++ 1.Common_Porblems/1Variable-conditions/1Positive_negative.ts
@@ -0,0 +1,8 @@
+let num:number=-5;
+
+console.log("Number is: ",num);
+
+if(num>0) console.log("positive number");
+else if(num<0) console.log("negative number");
+else console.log("the number is zero");
+
