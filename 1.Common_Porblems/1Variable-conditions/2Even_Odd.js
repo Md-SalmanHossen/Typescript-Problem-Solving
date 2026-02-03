ef2aea43fde2fcb7a5a03c7ee1a0e77fe894d94b
SalmanHossen <hossainsalman873@gmail.com>
2026-02-03 22:40:48 +0600

    practice problem even odd in ts

---
+++ 1.Common_Porblems/1Variable-conditions/2Even_Odd.js
@@ -0,0 +1,6 @@
+var num = 21;
+console.log("Given number is: ", num);
+if (num % 2 === 0)
+    console.log("Even number");
+else
+    console.log("Odd number");
---
+++ 1.Common_Porblems/1Variable-conditions/2Even_Odd.ts
@@ -0,0 +1,5 @@
+const num:number=21;
+console.log("Given number is: ",num);
+
+if(num%2===0) console.log("Even number");
+else console.log("Odd number");
