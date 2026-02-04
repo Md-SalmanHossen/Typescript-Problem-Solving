ab4917cadb7ddb5309e0e4863e2628d40486ba25
SalmanHossen <hossainsalman873@gmail.com>
2026-02-04 11:05:56 +0600

    solve max in 2 numbers

---
+++ 1.Common_Porblems/1Variable-conditions/3FindMax.js
@@ -0,0 +1,9 @@
+var num1 = 333;
+var num2 = 444;
+console.log("Given numbers are ".concat(num1, " and ").concat(num2));
+if (num1 > num2)
+    console.log("".concat(num1, " is a max number"));
+else if (num1 === num2)
+    console.log("".concat(num1, " and ").concat(num2, " are equal numbers"));
+else
+    console.log("".concat(num2, " is a max number"));
---
+++ 1.Common_Porblems/1Variable-conditions/3FindMax.ts
@@ -0,0 +1,9 @@
+const num1:number=333;
+const num2:number=444;
+
+console.log(`Given numbers are ${num1} and ${num2}`);
+
+if(num1>num2) console.log(`${num1} is a max number`);
+else if (num1===num2) console.log(`${num1} and ${num2} are equal numbers`);
+else console.log(`${num2} is a max number`);
+
