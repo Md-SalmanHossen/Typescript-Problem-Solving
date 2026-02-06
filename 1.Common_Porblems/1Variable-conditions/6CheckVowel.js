afd06d95f5e4a97117cb633d76e07c79c875afa0
SalmanHossen <hossainsalman873@gmail.com>
2026-02-06 08:49:21 +0600

    solve vowel and constant

---
+++ 1.Common_Porblems/1Variable-conditions/6CheckVowel.js
@@ -0,0 +1,6 @@
+var str = 'd';
+console.log("Given string :", str);
+if (str === 'a' || str === 'e' || str === 'i' || str === 'o' || str === 'u' || str === 'A' || str === 'E' || str === 'I' || str === 'O' || str === 'U')
+    console.log("".concat(str, " is a vowel"));
+else
+    console.log("".concat(str, " is a constant"));
---
+++ 1.Common_Porblems/1Variable-conditions/6CheckVowel.ts
@@ -0,0 +1,10 @@
+const str:string='a';
+console.log("Given string :",str);
+
+
+if(str==='a'||str==='e'||str==='i'||str==='o'||str==='u' ||str==='A'||str==='E'||str==='I'||str==='O'||str==='U')
+   console.log(`${str} is a vowel`);
+else console.log(`${str} is not a string`)
+
+
+   
