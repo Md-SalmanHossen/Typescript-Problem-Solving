57b078539449a3987cbccb90e451ba3843c715e6
SalmanHossen <hossainsalman873@gmail.com>
2026-02-07 07:41:35 +0600

    solve a divisble by number by ts

---
+++ 1.Common_Porblems/1Variable-conditions/7Divisible.js
@@ -0,0 +1,5 @@
+function isDivisible(num) {
+    return num % 3 === 0 && num % 5 === 0;
+}
+console.log(isDivisible(15));
+console.log(isDivisible(12));
---
+++ 1.Common_Porblems/1Variable-conditions/7Divisible.ts
@@ -0,0 +1,8 @@
+
+
+function isDivisible(num:number):boolean{
+   return num%3===0 && num%5===0;
+}
+
+console.log(isDivisible(15));
+console.log(isDivisible(12));
