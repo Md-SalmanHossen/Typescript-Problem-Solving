ec3d419cbae571629e1b7d84296403a441d27dca
SalmanHossen <hossainsalman873@gmail.com>
2026-02-09 07:41:18 +0600

    solve sum of array

---
+++ 1.Common_Porblems/2Loops_number/1SumOfArry.js
@@ -0,0 +1,6 @@
+var array = [12, 123, 44, 55, 5.77, 88];
+var sum = 0;
+for (var i = 0; i < array.length; i++) {
+    sum = sum + array[i];
+}
+console.log("total sum: ", sum);
---
+++ 1.Common_Porblems/2Loops_number/1SumOfArry.ts
@@ -0,0 +1,11 @@
+const array:number[]=[12,123,44,55,5.77,88];
+
+let sum:number=0;
+
+for(let i=0;i<array.length;i++){
+    sum=sum+array[i];
+}
+
+console.log("total sum: ",sum);
+
+
