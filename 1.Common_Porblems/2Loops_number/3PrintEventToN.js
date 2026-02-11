d42a228b2c2da6d22778468d1213aca53e720c3a
SalmanHossen <hossainsalman873@gmail.com>
2026-02-11 21:07:21 +0600

    solve print even to N

---
+++ 1.Common_Porblems/2Loops_number/3PrintEventToN.js
@@ -0,0 +1,7 @@
+function printNumber(n) {
+    for (var i = 0; i < n; i++) {
+        if (i % 2 === 0)
+            console.log(i);
+    }
+}
+printNumber(10);
---
+++ 1.Common_Porblems/2Loops_number/3PrintEventToN.ts
@@ -0,0 +1,8 @@
+
+function printNumber(n:number):void{
+   for(let i:number=0;i<n;i++){
+      if(i%2===0) 
+         console.log(i);
+   }
+}
+printNumber(10);
